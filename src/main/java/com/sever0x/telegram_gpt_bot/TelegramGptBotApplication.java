1dc39b8dde420e33eb04be02a45e56305d3ec6cb
shdwraze <shdwrazedev@gmail.com>
2023-11-16 19:39:20 +0200

    Init project

---
+++ src/main/java/com/sever0x/telegram_gpt_bot/TelegramGptBotApplication.java
@@ -0,0 +1,13 @@
+package com.sever0x.telegram_gpt_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramGptBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramGptBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sever0x/telegram_gpt_bot/TelegramGptBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sever0x.telegram_gpt_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramGptBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
